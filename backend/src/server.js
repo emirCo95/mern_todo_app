baeac755ced381e542ba5d7e1c75e117d1705382
Emir Cokrlija <144956911+emirCo95@users.noreply.github.com>
2025-05-30 16:08:07 -0400

    create Note.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,11 +7,12 @@ import notesRoutes from './routes/notesRoutes.js';
 import { connectDB } from './config/db.js';
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 connectDB();
 
 app.use('/api/notes', notesRoutes);
 
-app.listen(5000, () => {
+app.listen(PORT, () => {
   console.log('Server is running on port 5000');
 });
