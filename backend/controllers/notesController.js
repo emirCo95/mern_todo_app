64fdcb46894e78a24d2cad723eb16a9a03b0ca7e
Emir Cokrlija <144956911+emirCo95@users.noreply.github.com>
2025-05-30 13:35:00 -0400

    Update notesController.js

--- backend/controllers/notesController.js
+++ backend/controllers/notesController.js
@@ -1,3 +1,7 @@
 export const getAllNotes = (req, res) => {
   res.status(200).json({ message: 'Welcome to the Notes API!' });
 };
+
+export const createNote = (req, res) => {
+  res.status(201).json({ message: 'Note created successfully!' });
+};
