662e09518a182e1810a94e3f660c69feae7c350e
Emir Cokrlija <144956911+emirCo95@users.noreply.github.com>
2025-05-30 12:17:33 -0400

    npm i nodemon

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,10 @@ import express from 'express';
 
 const app = express();
 
+app.get('/api/notes', (req, res) => {
+  res.json('You got five notes.');
+});
+
 app.listen(5000, () => {
   console.log('Server is running on port 5000');
 });
