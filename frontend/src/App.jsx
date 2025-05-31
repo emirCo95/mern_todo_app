effa16b25cc5142b58567de5818fb8f0dea6e91e
Emir Cokrlija <144956911+emirCo95@users.noreply.github.com>
2025-05-31 15:31:24 -0400

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,15 @@
-import React from 'react';
+import { Route, Routes } from 'react-router';
+
+import HomePage from './pages/HomePage';
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
