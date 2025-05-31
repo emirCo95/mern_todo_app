457335581e1384d73f2c5cf7c9db9970fe68a919
Emir Cokrlija <144956911+emirCo95@users.noreply.github.com>
2025-05-31 18:41:37 -0400

    npm i cors

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import rateLimiter from './middleware/rateLimiter.js';
+import cors from 'cors';
 
 dotenv.config();
 
@@ -12,6 +13,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(rateLimiter);
+app.use(cors());
 
 app.use('/api/notes', notesRoutes);
 
