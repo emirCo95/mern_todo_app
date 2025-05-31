42b2f1cc393c718202f4c4036ff5404c0d07d9f8
Emir Cokrlija <144956911+emirCo95@users.noreply.github.com>
2025-05-31 18:34:33 -0400

    create RateLimiterUI.jsx

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
+import RateLimiterUI from '../components/RateLimiterUI';
 
 const HomePage = () => {
+  const [isRateLimited, setIsRateLimited] = React.useState(false);
   return (
     <div className="min-h-screen">
       <Navbar />
+      {isRateLimited && <RateLimiterUI />}
     </div>
   );
 };
