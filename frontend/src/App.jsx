ee38aca56f2ba27b35b4d7c32bd9dda497f5413b
Emir Cokrlija <144956911+emirCo95@users.noreply.github.com>
2025-05-31 15:48:56 -0400

    daisyui theme

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,7 +6,8 @@ import NoteDetailPage from './pages/NoteDetailPage';
 
 const App = () => {
   return (
-    <div>
+    <div data-theme="forest">
+      <button className="btn btn-outline">Click Me</button>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/create" element={<CreatePage />} />
