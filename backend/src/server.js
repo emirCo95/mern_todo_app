3def4d7a459ed1e5d319667532f29299e61d92b2
Emir Cokrlija <144956911+emirCo95@users.noreply.github.com>
2025-05-31 11:31:21 -0400

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -10,13 +10,13 @@ import { connectDB } from './config/db.js';
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-connectDB();
-
 app.use(express.json());
 app.use(rateLimiter);
 
 app.use('/api/notes', notesRoutes);
 
-app.listen(PORT, () => {
-  console.log('Server is running on port 5000');
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log('Server is running on port 5000');
+  });
 });
