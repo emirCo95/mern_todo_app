cb8d0c3f2d724799239620b1929bac61998c3ac1
Emir Cokrlija <144956911+emirCo95@users.noreply.github.com>
2025-05-31 18:21:54 -0400

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,7 +7,6 @@ import NoteDetailPage from './pages/NoteDetailPage';
 const App = () => {
   return (
     <div data-theme="forest">
-      <button className="btn btn-outline">Click Me</button>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/create" element={<CreatePage />} />
