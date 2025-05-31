f83fc01f21b90b07c7e80e5e37ae32a8c759f011
Emir Cokrlija <144956911+emirCo95@users.noreply.github.com>
2025-05-31 18:48:06 -0400

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -11,9 +11,9 @@ import { connectDB } from './config/db.js';
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(cors());
 app.use(express.json());
 app.use(rateLimiter);
-app.use(cors());
 
 app.use('/api/notes', notesRoutes);
 
