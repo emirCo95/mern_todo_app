8345deb3a3496f92c7d29c234bb5beb588a056cb
Emir Cokrlija <144956911+emirCo95@users.noreply.github.com>
2025-06-01 09:39:00 -0400

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import rateLimiter from './middleware/rateLimiter.js';
 import cors from 'cors';
+import path from path
 
 dotenv.config();
 
@@ -10,12 +11,21 @@ import { connectDB } from './config/db.js';
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
 app.use(cors());
 app.use(express.json());
 app.use(rateLimiter);
 
 app.use('/api/notes', notesRoutes);
+
+if(process.env.NODE_ENV === 'production'){
+  app.use(express.static(path.join(__dirname, '../frontend/dist')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
+  })
+}
 
 connectDB().then(() => {
   app.listen(PORT, () => {
