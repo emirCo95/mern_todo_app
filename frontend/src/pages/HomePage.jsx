2354016f752aa0c999bc1949432bced88e4a3e8c
Emir Cokrlija <144956911+emirCo95@users.noreply.github.com>
2025-06-01 07:29:25 -0400

    create NotesNotFound.jsx

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -44,6 +44,8 @@ const HomePage = () => {
       <Navbar />
       {isRateLimited && <RateLimiterUI />}
       <div className="max-w-7xl mx-auto p-4 mt-6">
+        {notes.length === 0 && !isRateLimited && <NotesNotFound />}
+
         {notes.length > 0 && !isRateLimited && (
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
             {notes.map((note) => (
